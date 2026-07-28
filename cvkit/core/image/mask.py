ba568291a53b90bc39574c64ddf27e666bb85d79
qzq <619579060@qq.com>
2026-07-28 15:02:16 +0800

    Update: MaskImageUtils添加generate_from_hbb方法

--- cvkit/core/image/mask.py
+++ cvkit/core/image/mask.py
@@ -26,7 +26,7 @@ class MaskImageUtils(ImageIO):
         if self.erase_num < 0:
             raise ValueError("erase_num must be >= 0")
 
-    def generate(self) -> "MaskImageUtils":
+    def generate_from_seg(self) -> "MaskImageUtils":
         mask = np.zeros((self.height, self.width), dtype=np.uint8)
         lines = TXTDocument(self.label_path).readlines()
         if len(lines) == 0:
@@ -35,8 +35,7 @@ class MaskImageUtils(ImageIO):
         polygons = []
         for line in lines:
             values = line.strip().split()
-            if int(values[0]) != self.cls:
-                continue
+            if int(values[0]) != self.cls: continue
             if len(values) < 7:
                 raise ValueError(f"At least 7 words in label_path: {self.label_path}")
             if (len(values) - 1) % 2 != 0:
@@ -58,6 +57,44 @@ class MaskImageUtils(ImageIO):
         selected_indices = rng.choice(len(polygons), size=self.erase_num, replace=False)
         for index in selected_indices:
             cv2.fillPoly(mask, [polygons[index]], 255)
+
+        save_path = self.path.parents[1] / "mask" / f"{self.path.stem}.png"
+        super().save(mask, save_path)
+        return self
+
+    def generate_from_hbb(self) -> "MaskImageUtils":
+        mask = np.zeros((self.height, self.width), dtype=np.uint8)
+        lines = TXTDocument(self.label_path).readlines()
+        if len(lines) == 0:
+            raise ValueError("label cat not be empty")
+
+        bboxes = []
+        for line in lines:
+            values = line.strip().split()
+            class_id, x_center, y_center, box_width, box_height = values
+            if int(class_id) != self.cls: continue
+
+            x_center, y_center, box_width, box_height = float(x_center), float(y_center), float(box_width), float(box_height)
+
+            x1 = round((x_center - box_width / 2) * self.width)
+            y1 = round((y_center - box_height / 2) * self.height)
+            x2 = round((x_center + box_width / 2) * self.width)
+            y2 = round((y_center + box_height / 2) * self.height)
+
+            x1 = int(np.clip(x1, 0, self.width - 1))
+            y1 = int(np.clip(y1, 0, self.height - 1))
+            x2 = int(np.clip(x2, 0, self.width - 1))
+            y2 = int(np.clip(y2, 0, self.height - 1))
+
+            bboxes.append(((x1, y1), (x2, y2)))
+
+        if self.erase_num > len(bboxes):
+            raise ValueError(f"erase_num={self.erase_num} 超过标注数量 {len(bboxes)}")
+        rng = np.random.default_rng()
+        selected_indices = rng.choice(len(bboxes), size=self.erase_num, replace=False)
+        for index in selected_indices:
+            top_left, bottom_right = bboxes[index]
+            cv2.rectangle(mask, top_left, bottom_right, color=255, thickness=-1)
 
         save_path = self.path.parents[1] / "mask" / f"{self.path.stem}.png"
         super().save(mask, save_path)
